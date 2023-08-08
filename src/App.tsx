b99bcb450fdbc9b01fabf2afd25d15ec5b58d2da
iadaria <jadarya@mail.ru>
2023-08-08 17:03:37 +0900

    comment

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,33 @@
  */
 
 import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
   ScrollView,
   StatusBar,
-  StyleSheet,
   Text,
   useColorScheme,
   View,
 } from 'react-native';
 
-type SectionProps = PropsWithChildren<{
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <SafeAreaView>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView contentInsetAdjustmentBehavior="automatic">
+        <View>
+          <Text>Hi</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+//import type {PropsWithChildren} from 'react';
+
+/* type SectionProps = PropsWithChildren<{
   title: string;
 }>;
 
@@ -28,32 +43,8 @@ function Section({children, title}: SectionProps): JSX.Element {
       <Text style={[styles.sectionDescription]}>{children}</Text>
     </View>
   );
-}
-
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  return (
-    <SafeAreaView>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView contentInsetAdjustmentBehavior="automatic">
-        <View>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes"></Section>
-          <Section title="Debug"></Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
+} */
+/* const styles = StyleSheet.create({
   sectionContainer: {
     marginTop: 32,
     paddingHorizontal: 24,
@@ -70,6 +61,6 @@ const styles = StyleSheet.create({
   highlight: {
     fontWeight: '700',
   },
-});
+}); */
 
 export default App;
