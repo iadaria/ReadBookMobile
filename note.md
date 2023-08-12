5fc982b1ddd6a16190907676fb0c7a3d78a8f5e4
iadaria <jadarya@mail.ru>
2023-08-12 16:00:22 +0900

    without keys

--- note.md
+++
@@ -1,2 +0,0 @@
-https://dev.to/mikehamilton00/adding-web-support-to-a-react-native-project-in-2023-4m4l
-https://retool.com/blog/how-to-make-your-react-native-apps-work-on-the-web/
--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,14 @@ import {wordReducer} from './store/book.reducer';
 
 function App(): JSX.Element {
   const [wordState, wordDispatch] = useReducer(wordReducer, {word: null});
+
+  console.log('NODE_ENV', process.env.NODE_ENV);
+
   const providerState = {
     wordState,
     wordDispatch,
   };
+
   return (
     <WordContext.Provider value={providerState}>
       <MainScreen />
